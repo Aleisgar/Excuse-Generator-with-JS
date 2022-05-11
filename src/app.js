cee31e63321a6ad74b131006cf528dadb255fac7
Aleisgar <aleisgar@gmail.com>
2022-05-11 18:16:52 +0000

    Upload

--- src/app.js
+++ src/app.js
@@ -6,6 +6,31 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector("#excuse").innerHTML = generateExcuse();
+  console.log(generateExcuse());
 };
+
+let who = ["My neighbour", "My grandma", "A snail", "His snake"];
+let action = [" ate", " fell into", " bit", " ran over"];
+let what = [" my food", " a chair", " my bike", " a cat"];
+let when = [
+  " when going for a walk",
+  " when I arrived",
+  " while I was sleeping",
+  " during my classes",
+  " at night"
+];
+// ejercicio con código extendido. Provar con código optimizado
+
+const generateExcuse = arr => {
+  let indexWho = Math.floor(Math.random(who) * who.length);
+  let indexAction = Math.floor(Math.random(action) * action.length);
+  let indexWhat = Math.floor(Math.random(what) * what.length);
+  let indexWhen = Math.floor(Math.random(when) * when.length);
+
+  return (
+    who[indexWho] + action[indexAction] + what[indexWhat] + when[indexWhen]
+  );
+};
+
+console.log(generateExcuse());
